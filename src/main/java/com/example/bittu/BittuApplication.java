b6015eb7dd482f5d8f58225df571d2514389dbc5
unknown <20it110@charusat.edu.in>
2022-09-06 13:24:08 +0530

    first commit

---
+++ src/main/java/com/example/bittu/BittuApplication.java
@@ -0,0 +1,13 @@
+package com.example.bittu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BittuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BittuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bittu/BittuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bittu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BittuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
